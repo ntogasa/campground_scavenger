7e945f93c27d1dd3cdeabafc43dbd20df3d61850
Nikk Ogasa <ntogasa@gmail.com>
2020-09-14 21:20:46 -0700

    Implement a Vue app to display campground IDs
    - Add vue-clipboard to static/js
    - Create a Vue instance in main.js that takes in a string and returns a list of campgrounds
    - Add a copy-to-clipboard button

--- apps/library/views.py
+++ apps/library/views.py
@@ -6,7 +6,7 @@ from .models import Campground, Log
 # Create your views here.
 def list_view(request):
     campgrounds = Campground.objects.all()
-    return render(request, 'list.html', {'campgrounds': campgrounds})
+    return render(request, 'list.html')
 
 
 def scrape_view(request):
