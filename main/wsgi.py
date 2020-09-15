82669d33542c1f6e0635e3a0ea5464aab84d908b
Nikk Ogasa <ntogasa@gmail.com>
2020-09-15 12:43:05 -0700

    Set up separate prod and dev settings files

--- main/wsgi.py
+++ main/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.development')
 application = get_wsgi_application()
 
 application = WhiteNoise(application, root=os.path.join('static'))
