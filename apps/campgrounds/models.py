4d63ec25e525274c8c19cd1a24a35cfaa884e91d
Nikk Ogasa <ntogasa@gmail.com>
2020-09-17 22:03:47 -0700

    Extend max char lengh of camp_id to 10

--- apps/campgrounds/models.py
+++ apps/campgrounds/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Campground(models.Model):
-    camp_id = models.CharField(max_length=6)
+    camp_id = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
     parent = models.CharField(max_length=10000)
 
